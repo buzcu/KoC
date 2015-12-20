0d0d69d6a07216bc6a042afdd6abac495ca393cc
buzcu <gorkem.buzcu@gmail.com>
2015-12-20 17:09:41 +0200

    progress

--- koc/KnightsofCydonia.java
+++ koc/KnightsofCydonia.java
@@ -2,49 +2,106 @@ package koc;
 
 import java.util.Scanner;
 
-/**
- * @author gorkem
- *
- */
 public class KnightsofCydonia {
 
 	private static Scanner reader;
 
+	private static String getPlayerTypeFromUser()
+	{
+		String playerType = "-1";
+		int typeValid = 0;
+
+		while (typeValid == 0)
+		{
+			System.out.println("Please select your character:");
+			System.out.println("1. Hunter:");
+			System.out.println("2. Knight:");
+			System.out.println("3. Ninja:");
+			System.out.println("4. Warrior:");
+			System.out.println("5. Mage:");
+			System.out.println("6. Orc:");
+
+			reader = new Scanner(System.in);
+			playerType = reader.next();
+
+			if (playerType != "1" &&
+					playerType != "2" &&
+					playerType != "3" &&
+					playerType != "4" &&
+					playerType != "5" &&
+					playerType != "6")
+			{
+				System.out.println("You made an incorrect entry!");
+			}
+			else
+			{
+				typeValid = 1;
+			}
+		}
+
+		return playerType;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("\n/------\t------\t------\\\n*/\\*Knights of Cydonia*/\\*\n/------\t------\t------\\");
-		System.out.println("Please enter your name:");
-		reader = new Scanner(System.in);
-		String s = reader.next();
-		System.out.println("Hello "+s);
-		System.out.println(s+" please choose a character:\npress 1 for Knight\n" +
-				"press 2 for Ninja\n" +
-				"press 3 for Mage\n" +
-				"press 4 for Hunter\n" +
-				"press 5 for Warrior");
-		String choice = reader.next();
-		if (choice.equals("1")){
-			player = new koc.Knight();
-		}
-		else if (choice.equals("2")){
-			player = new koc.Ninja();
-		}
 
-		else if (choice.equals("3")){
-			player = new koc.Mage();
-		}
+		String playerName = getPlayerNameFromUser();
+		String playerType = getPlayerTypeFromUser();
+		Hero player = getPlayerObject(playerName, playerType);
 
-		else if (choice.equals("4")){
-			player = new koc.Hunter();
-		}
+		GameEnvironment gameEnvironment = new GameEnvironment();
 
-		else if (choice.equals("5")){
-			player = new koc.Warrior();
-		}
-
-		
-		
 
 	}
-	
+
+	private static Hero getPlayerObject(String playerName, String playerType) {
+		Hero player;
+		if (playerType == "1")
+		{
+			System.out.println("You selected Hunter");
+			player = new Hunter();
+		}
+		else if (playerType == "2")
+		{
+			System.out.println("You selected Knight");
+			player = new Knight();
+		}
+		else if (playerType == "3")
+		{
+			System.out.println("You selected Ninja");
+			player = new Ninja();
+		}
+		else if (playerType == "4")
+		{
+			System.out.println("You selected Warrior");
+			player = new Warrior();
+		}
+		else if (playerType == "5")
+		{
+			System.out.println("You selected Mage");
+			player = new Mage();
+		}
+		else if (playerType == "6")
+		{
+			System.out.println("You selected Orc");
+			player = new Orc();
+		}
+		else
+		{
+			return null;
+		}
+		return player;
+	}
+
+	private static String getPlayerNameFromUser() {
+
+		System.out.println("Please enter your name:");
+
+		reader = new Scanner(System.in);
+		String playerName = reader.next();
+		System.out.println("Hello "+playerName);
+
+		return playerName;
+	}
+
 }
