1b0775ad2810908aa9c588d25c19d7615a2758e1
Bora Kirca <kirca.bora@metu.edu.tr>
2015-12-20 19:01:39 +0200

    bora: modified main and game cycle. compiles but not complete yet.

--- koc/GameEnvironment.java
+++ koc/GameEnvironment.java
@@ -1,23 +1,110 @@
-/**
- * 
- */
+
 package koc;
 
-/**
- * @author gorkem
- *
- */
+import java.util.Scanner;
+
+
 public class GameEnvironment {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static Scanner reader;
+	protected Hero player;
+	
+	public GameEnvironment()
+	{
+		reader = new Scanner(System.in);
+		String playerName = getPlayerNameFromUser();
+		String playerType = getPlayerTypeFromUser();
+		player = getPlayerObject(playerName, playerType);
+		reader.close();
+		
+		
 	}
-	public void createEnvironment(){
-		//TODO create environment somehow
+
+
+	private static String getPlayerNameFromUser() {
+
+		System.out.println("Please enter your name:");
+
+		String playerName = reader.next();
+		System.out.println("Hello "+playerName);
+
+		return playerName;
 	}
 
+	private static String getPlayerTypeFromUser()
+	{
+		String playerType = "-1";
+		int typeValid = 0;
+		
+		while (typeValid == 0)
+		{
+			System.out.println("Please select your character:");
+			System.out.println("1. Hunter:");
+			System.out.println("2. Knight:");
+			System.out.println("3. Ninja:");
+			System.out.println("4. Warrior:");
+			System.out.println("5. Mage:");
+			System.out.println("6. Orc:");
+			
+			
+			playerType = reader.next();
+
+			if (
+					playerType.equals("1") ||
+					playerType.equals("2") ||
+					playerType.equals("3") ||
+					playerType.equals("4") ||
+					playerType.equals("5") ||
+					playerType.equals("6")
+				)
+			{
+				typeValid = 1;
+				
+			}
+			else
+			{
+				System.out.println("You made an incorrect entry!");
+			}
+		}
+		return playerType;
+	}
+
+	private static Hero getPlayerObject(String playerName, String playerType) {
+		Hero player;
+		if (playerType == "1")
+		{
+			System.out.println("You selected Hunter");
+			player = new Hunter();
+		}
+		else if (playerType == "2")
+		{
+			System.out.println("You selected Knight");
+			player = new Knight();
+		}
+		else if (playerType == "3")
+		{
+			System.out.println("You selected Ninja");
+			player = new Ninja();
+		}
+		else if (playerType == "4")
+		{
+			System.out.println("You selected Warrior");
+			player = new Warrior();
+		}
+		else if (playerType == "5")
+		{
+			System.out.println("You selected Mage");
+			player = new Mage();
+		}
+		else if (playerType == "6")
+		{
+			System.out.println("You selected Orc");
+			player = new Orc();
+		}
+		else
+		{
+			return null;
+		}
+		return player;
+	}
+	
 }
--- koc/KnightsofCydonia.java
+++ koc/KnightsofCydonia.java
@@ -1,108 +1,16 @@
 package koc;
 
-import java.util.Scanner;
-
 public class KnightsofCydonia {
 
-	private static Scanner reader;
+	private static GameEnvironment game;
 
-	private static String getPlayerTypeFromUser()
-	{
-		String playerType = "-1";
-		int typeValid = 0;
-
-		while (typeValid == 0)
-		{
-			System.out.println("Please select your character:");
-			System.out.println("1. Hunter:");
-			System.out.println("2. Knight:");
-			System.out.println("3. Ninja:");
-			System.out.println("4. Warrior:");
-			System.out.println("5. Mage:");
-			System.out.println("6. Orc:");
-
-			reader = new Scanner(System.in);
-			playerType = reader.next();
-
-			if (playerType.equals("1") ||
-					playerType.equals("2") ||
-					playerType.equals("3") ||
-					playerType.equals("4") ||
-					playerType.equals("5")||
-					playerType.equals("6"))
-			{
-				typeValid = 1;
-				
-			}
-			else
-			{
-				System.out.println("You made an incorrect entry!");
-			}
-		}
-
-		return playerType;
-	}
-
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("\n/------\t------\t------\\\n*/\\*Knights of Cydonia*/\\*\n/------\t------\t------\\");
+		game = new GameEnvironment();
 
-		String playerName = getPlayerNameFromUser();
-		String playerType = getPlayerTypeFromUser();
-		Hero player = getPlayerObject(playerName, playerType);
-
-		GameEnvironment gameEnvironment = new GameEnvironment();
-
-
-	}
-
-	private static Hero getPlayerObject(String playerName, String playerType) {
-		Hero player;
-		if (playerType == "1")
-		{
-			System.out.println("You selected Hunter");
-			player = new Hunter();
-		}
-		else if (playerType == "2")
-		{
-			System.out.println("You selected Knight");
-			player = new Knight();
-		}
-		else if (playerType == "3")
-		{
-			System.out.println("You selected Ninja");
-			player = new Ninja();
-		}
-		else if (playerType == "4")
-		{
-			System.out.println("You selected Warrior");
-			player = new Warrior();
-		}
-		else if (playerType == "5")
-		{
-			System.out.println("You selected Mage");
-			player = new Mage();
-		}
-		else if (playerType == "6")
-		{
-			System.out.println("You selected Orc");
-			player = new Orc();
-		}
-		else
-		{
-			return null;
-		}
-		return player;
-	}
-
-	private static String getPlayerNameFromUser() {
-
-		System.out.println("Please enter your name:");
-
-		reader = new Scanner(System.in);
-		String playerName = reader.next();
-		System.out.println("Hello "+playerName);
-
-		return playerName;
 	}
 
 }
