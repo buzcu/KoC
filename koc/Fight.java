4a42fd97323e6f49f537eba4f4b61958a04b13b4
Alican Karamil <alicankaramil@gmail.com>
2016-01-09 00:07:44 +0200

    no message

--- koc/Fight.java
+++ koc/Fight.java
@@ -1,5 +1,6 @@
 package koc;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Fight {
@@ -15,7 +16,12 @@ public class Fight {
 
     public Boolean main(Character player, Character enemy){
     	String choice;
+    	Random rand = new Random();
+    	double attackSpeedTotal = player.attackSpeed + enemy.attackSpeed;
+    	attackSpeedTotal *= 100;
         while (player.health>0 && enemy.health>0){
+        	if (rand.nextInt((int) attackSpeedTotal) > enemy.attackSpeed*100)
+        	{
             System.out.println("\nIt is your turn. Do you want to (H)it or (R)un:");
             choice = reader.next();
             if (choice.equals("r")){
@@ -25,20 +31,19 @@ public class Fight {
             }
 
             player.attack_to(enemy);
-            if (enemy.health>0) {
+        	}
+        	else
+        	{
                 System.out.println("\nIt is your enemy's turn to attack!");
                 enemy.attack_to(player);
             }
-            else {
-                player.collectChest();
-                player.getExperience(3);
-                System.out.println("Your current health is "+player.health+", Your maximum health is "+player.maxHealth);
-            }
         }
         if (player.health>0){
-            return Boolean.TRUE;
+    	   player.collectChest();
+           player.getExperience(3);
+           System.out.println("Your current health is "+player.health+"/"+player.maxHealth);
+           return Boolean.TRUE;
         }
-
         else {
             System.out.println("Oh No! You have been slayed by enemy! Your journey is over. Better luck next time. \n(Game Over)");
             return Boolean.FALSE;
